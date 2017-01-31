8f4946ea144afb1604aab3c8bdcad44380eca723
lucas chapdelaine <lucas.chapdelaine@laposte.net>
2017-01-31 13:39:47 +0100

    creation affichage

--- AffichageUserflux/ClassLibrary1/Properties/AssemblyInfo.cs
+++ AffichageUserflux/ClassLibrary1/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AfficgaheUserflux")]
+[assembly: AssemblyTitle("ClassLibrary1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AfficgaheUserflux")]
+[assembly: AssemblyProduct("ClassLibrary1")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("28644a3d-626c-4d66-8d1a-a1e67b708384")]
+[assembly: Guid("d53278e6-4161-45a1-8087-121aefb0e333")]
 
 // Version information for an assembly consists of the following four values:
 //
