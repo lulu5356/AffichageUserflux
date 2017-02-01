0e090e3d26846e3eb8422d2b68746cd596bbcffd
Florian LE GOFF <contact@florianlegoff.com>
2017-02-01 11:51:25 +0100

    Add login/register user functions

--- AffichageUserflux/AffichageUserflux/CreateUserWindow.xaml.cs
+++ AffichageUserflux/AffichageUserflux/CreateUserWindow.xaml.cs
@@ -10,26 +10,31 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Userflux;
 
 namespace AffichageUserflux
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for CreateUserWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class CreateUserWindow : Window
     {
-        public MainWindow()
+        public CreateUserWindow()
         {
             InitializeComponent();
         }
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
+            User u = new User();
+            u.Login = this.login.Text;
+            u.Password = this.password.Text;
+            u.Firstname = this.firstname.Text;
+            u.Lastname = this.lastname.Text;
+
             Mysql bdd = new Mysql();
-            User loggedUser = bdd.getUser(this.textBox.Text, this.passwordBox.Password);
+            bdd.AddUser(u);
 
             CreateUserWindow createUser = new CreateUserWindow();
             createUser.Show();
--- AffichageUserflux/AffichageUserflux/Mysql.cs
+++ AffichageUserflux/AffichageUserflux/Mysql.cs
@@ -11,54 +11,41 @@ namespace AffichageUserflux
     class Mysql
     {
         private MySqlConnection connection;
-
-        // Constructeur
+        
         public Mysql()
         {
-            this.InitConnexion();
-        }
-
-        // Méthode pour initialiser la connexion
-        private void InitConnexion()
-        {
-            // Création de la chaîne de connexion
-            string connectionString = "SERVER=127.0.0.1; DATABASE=mli; UID=root; PASSWORD=";
+            string connectionString = "SERVER=127.0.0.1; DATABASE=csharp; UID=root; PASSWORD=";
             this.connection = new MySqlConnection(connectionString);
         }
 
-        // Méthode pour ajouter un contact
         public void AddUser(User user)
         {
-            try
+            this.connection.Open();
+            MySqlCommand cmd = this.connection.CreateCommand();
+            cmd.CommandText = "INSERT INTO app_user (login, password, firstname, lastname) VALUES (@login, @password, @firstname, @lastname)";
+            cmd.Parameters.AddWithValue("@login", user.Login);
+            cmd.Parameters.AddWithValue("@password", user.Password);
+            cmd.Parameters.AddWithValue("@firstname", user.Firstname);
+            cmd.Parameters.AddWithValue("@lastname", user.Lastname);
+            cmd.ExecuteNonQuery();
+            this.connection.Close();
+        }
+
+        public User getUser(string login, string password)
+        {
+            this.connection.Open();
+            MySqlCommand cmd = this.connection.CreateCommand();
+            cmd.CommandText = "SELECT * FROM app_user WHERE login = '"+ login +"' AND password = '" + password + "'";
+            MySqlDataReader reader = cmd.ExecuteReader();
+            User u = new User();
+            while (reader.Read())
             {
-                // Ouverture de la connexion SQL
-                this.connection.Open();
-
-                // Création d'une commande SQL en fonction de l'objet connection
-                MySqlCommand cmd = this.connection.CreateCommand();
-
-                // Requête SQL
-                cmd.CommandText = "INSERT INTO app_user (login, password, firstname, lastname) VALUES (@login, @password, @firstname, @lastname)";
-
-                // utilisation de l'objet contact passé en paramètre
-                cmd.Parameters.AddWithValue("@id", user.Id);
-                cmd.Parameters.AddWithValue("@login", user.Login);
-                cmd.Parameters.AddWithValue("@password", user.Password);
-                cmd.Parameters.AddWithValue("@firstname", user.Firstname);
-                cmd.Parameters.AddWithValue("@lastname", user.Lastname);
-
-                // Exécution de la commande SQL
-                cmd.ExecuteNonQuery();
-
-                // Fermeture de la connexion
-                this.connection.Close();
+                u.Firstname = (string)reader["firstname"];
+                u.Lastname = (string)reader["lastname"];
             }
-            catch
-            {
-                // Gestion des erreurs :
-                // Possibilité de créer un Logger pour les exceptions SQL reçus
-                // Possibilité de créer une méthode avec un booléan en retour pour savoir si le contact à été ajouté correctement.
-            }
+            this.connection.Close();
+
+            return u;
         }
     }
 }
